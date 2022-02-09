245e9843565b10854c9ea63abfde4081338fbc2b
JonathanJFlores <jonathan099902@gmail.com>
2022-02-09 09:51:52 -0600

    api run port 4000

--- src/command.rs
+++ src/command.rs
@@ -10,7 +10,7 @@ pub struct Opt {
         default_value = "ws://127.0.0.1:9944"
     )]
     pub node_server: Url,
-    #[structopt(short = "l", long = "listen", default_value = "http://127.0.0.1:8010")]
+    #[structopt(short = "l", long = "listen", default_value = "http://127.0.0.1:4000")]
     pub listen: Url,
     #[structopt(short = "d", long = "db-uri")]
     pub db: Option<String>,
